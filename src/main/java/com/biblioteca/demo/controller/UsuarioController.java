21aab995ea3fe564077e1ed07046a481e887161a
alineCoutoDSouza <alinecouto337@gmail.com>
2024-09-13 01:32:16 -0300

    Feita autenticacao por JWT Tive que usar o video da fernanda kipper como base para poder aplicar o security com jwt

--- src/main/java/com/biblioteca/demo/controller/UsuarioController.java
+++ src/main/java/com/biblioteca/demo/controller/UsuarioController.java
@@ -1,9 +1,17 @@
 package com.biblioteca.demo.controller;
 
+import com.biblioteca.demo.entity.LoginDTO;
+import com.biblioteca.demo.entity.LoginResponseDTO;
+import com.biblioteca.demo.entity.RegisterDTO;
 import com.biblioteca.demo.entity.Usuario;
+import com.biblioteca.demo.security.TokenService;
 import com.biblioteca.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +25,12 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @GetMapping
     public List<Usuario> getAllUsuarios() {
         return usuarioService.findAll();
@@ -28,9 +42,28 @@ public class UsuarioController {
         return usuario.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping
-    public Usuario createUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.save(usuario);
+    // @PostMapping
+    // public Usuario createUsuario(@RequestBody Usuario usuario) {
+    //     return usuarioService.save(usuario);
+    // }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Validated LoginDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((Usuario)auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity login(@RequestBody RegisterDTO data){
+        if(usuarioService.loadUserByUsername(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        Usuario user = new Usuario(data.email(),encryptedPassword,data.role());
+
+        usuarioService.save(user);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
